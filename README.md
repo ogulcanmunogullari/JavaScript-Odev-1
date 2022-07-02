ba3684e826b7ded454cc0a2b2c57712eeab1f38c
Oğulcan Munoğulları <eeogulcan@gmail.com>
2022-07-02 16:06:35 +0400

    improving performance and better legibility

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1> 02.07.2022 Edit Notes;</h1>
+<h2>Before update; </h2>
+1)Whole section update itself every second. </br>
+2)Because i code like body.innerHTML = `everthing`.
+<h2> After update;</h2>
+1)I code just a changeble sections and i split them with spans. </br>
+2)With this just Second section updating every second, </br>  - Minute secton wait seconds to 00 and hour wait Minute section to 00. </br>
+3)Its seems using more and more less source.
+<h3>There is a little gif for the app; </h3>
+
+<img src="https://im4.ezgif.com/tmp/ezgif-4-912c995be7.gif" width="600px">
--- main.js
+++ main.js
@@ -1,28 +1,27 @@
-var name = prompt("Adınız nedir?");
-let main = document.querySelector("#body")
+const userName = prompt("Adınız nedir?");
 
-  function showTime() {
-   var date = new Date();
-   var h = date.getHours();
-   var m = date.getMinutes();
-   var s = date.getSeconds();
- 
-   h = h < 10 ? "0" + h : h;
-   m = m < 10 ? "0" + m : m;
-   s = s < 10 ? "0" + s : s;
- 
-   let time = h + ":" + m + ":" + s;
-
-   main.innerHTML = `
-   <div style="display:flex; justify-content:center; align-items:center;">
-        <div style="text-align:center">
-            <h1>Hoşgeldin ${(name)}</h1>
-            <h2>Şu anda saat ${time}</h2>
-            <h3>Front-End Development Patikası'nın JavaScript Bölümü, 1. Ödevindesin!</h3>
-        </div>
-   </div>
-`
-   setTimeout(showTime, 1000);
-  }
- 
-showTime();
+const minute = () => {
+  const date = new Date();
+  let m = date.getMinutes();
+  m = m < 10 ? "0" + m : m;
+  m == "00" ? hour() : m;
+  takeMinute.innerHTML = m;
+};
+const second = () => {
+  const date = new Date();
+  let s = date.getSeconds();
+  s = s < 10 ? "0" + s : s;
+  s == "00" ? minute() : s;
+  takeSecond.innerHTML = s;
+  setTimeout(second, 1000);
+};
+const hour = () => {
+  const date = new Date();
+  let h = date.getHours();
+  h = h < 10 ? "0" + h : h;
+  takeHour.innerHTML = h;
+};
+hour();
+minute();
+second();
+takeId.innerHTML = `Hoşgeldin ${userName}`;
